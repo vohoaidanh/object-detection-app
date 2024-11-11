85d07d20c7527dcc080b10fb67f037b4639c8be5
danh vo <danhvohoai2@gmail.com>
2024-11-11 22:54:26 +0700

    update

--- flask_backend/config.py
+++ flask_backend/config.py
@@ -1,2 +1,2 @@
 # flask_backend/config.py
-MODEL_SERVICE_URL = "http://localhost:8000/api/detect/"  # URL của FastAPI detection service
+MODEL_SERVICE_URL = "http://39.114.73.97:34188/api/detect/"  # URL của FastAPI detection service
